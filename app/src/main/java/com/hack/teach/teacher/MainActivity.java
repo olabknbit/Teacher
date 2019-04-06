18f3cfe06cb54dc600d9dc42873742eaaa0e3540
Aleksandra <al.byczynska@gmail.com>
2019-04-06 21:52:59 +0200

    refactor

--- app/src/main/java/com/hack/teach/teacher/MainActivity.java
+++ app/src/main/java/com/hack/teach/teacher/MainActivity.java
@@ -4,12 +4,10 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -109,8 +107,7 @@ public class MainActivity extends AppCompatActivity {
 
     private File createImageFile(String filename) throws IOException {
         // Create an image file name
-        // String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + filename + "_";
+        String imageFileName = filename + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(
                 imageFileName,  /* prefix */
@@ -131,16 +128,11 @@ public class MainActivity extends AppCompatActivity {
             File photoFile = null;
             try {
                 photoFile = createImageFile(filename);
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-
+            } catch (IOException e) {
+                e.printStackTrace();
             }
             // Continue only if the File was successfully created
             if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                 startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
             }
         }
